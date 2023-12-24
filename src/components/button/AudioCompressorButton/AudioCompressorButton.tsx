04e7d2f7dc05be3318aab04cb6563d5d1025c7f4
bluewarn <n5lp97@gmail.com>
2023-12-25 02:05:05 +0900

    Fix: AudioContext 를 User gesture 로 생성하도록 수정

--- src/components/button/AudioCompressorButton/AudioCompressorButton.tsx
+++ src/components/button/AudioCompressorButton/AudioCompressorButton.tsx
@@ -2,25 +2,29 @@ import "./AudioCompressorButton.css";
 
 export default function AudioCompressorButton() {
   const video = document.getElementsByTagName("video")[0];
-
   let audioCtx: AudioContext;
   let source: MediaElementAudioSourceNode;
   let compressor: DynamicsCompressorNode;
-
-  video.addEventListener('play', () => {
-    if (!audioCtx) {
-      audioCtx = new AudioContext();
-      source = audioCtx.createMediaElementSource(video);
-      compressor = audioCtx.createDynamicsCompressor();
-      source.connect(compressor);
-      compressor.connect(audioCtx.destination);
-    }
-  });
 
   const onClickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     const button = e.currentTarget;
     const active = button.getAttribute("data-active");
+
+    if(!audioCtx) {
+      audioCtx = new AudioContext();
+
+      source = audioCtx.createMediaElementSource(video);
+      compressor = audioCtx.createDynamicsCompressor();
+
+      compressor.threshold.setValueAtTime(-50, audioCtx.currentTime);
+      compressor.knee.setValueAtTime(40, audioCtx.currentTime);
+      compressor.ratio.setValueAtTime(12, audioCtx.currentTime);
+      compressor.attack.setValueAtTime(0, audioCtx.currentTime);
+      compressor.release.setValueAtTime(0.25, audioCtx.currentTime);
+
+      source.connect(audioCtx.destination);
+    }
 
     if (active === "false") {
       button.setAttribute("data-active", "true");
@@ -40,25 +44,8 @@ export default function AudioCompressorButton() {
   };
 
   return (
-    <button className="audio-compressor-button" onClick={onClickHandler}>
-      {/* <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="icon icon-tabler icon-tabler-pin-invoke"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        stroke-width="2"
-        stroke="currentColor"
-        fill="none"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      >
-        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-        <path d="M21 13v5a1 1 0 0 1 -1 1h-16a1 1 0 0 1 -1 -1v-12a1 1 0 0 1 1 -1h9" />
-        <path d="M19 7m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
-        <path d="M10 11h4v4" />
-        <path d="M10 15l4 -4" />
-      </svg> */}
+    <button className="audio-compressor-button" data-active='false' onClick={onClickHandler}>
+      Add compression
     </button>
   );
 }
