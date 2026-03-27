6952c67d5fef26e42e270ab625da4526d40ac35a
kyechan99 <kyechan99@gmail.com>
2026-03-27 18:45:19 +0900

    fix: create duplicates global

--- src/page/global.ts
+++ src/page/global.ts
@@ -13,7 +13,7 @@ export const editGlobalPage = () => {
    * 글로벌로 적용시킬만한 내용을 관리합니다.
    * 차후 페이지에 popup과 동일한 컴포넌트를 띄우는데 사용될 수 있어요.
    */
-  if (!document.getElementById('chzzk-plus-global')) {
+  if (!document.getElementById('czp-global')) {
     const $global = document.createElement('div');
     $global.id = 'czp-global';
     document.body.appendChild($global);
@@ -25,7 +25,7 @@ export const editGlobalPage = () => {
    * 세팅 버튼
    */
   const $globalToolbar = document.querySelector(SECTION_TOOLBAR);
-  if ($globalToolbar && !document.getElementById('chzzk-plus-golbal-toolbar')) {
+  if ($globalToolbar && !document.getElementById('czp-golbal-toolbar')) {
     const $tools = document.createElement('div');
     $tools.id = 'czp-golbal-toolbar';
     $globalToolbar?.prepend($tools);
